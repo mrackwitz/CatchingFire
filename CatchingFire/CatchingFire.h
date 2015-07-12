88be1dadcf7792bf070909b92f49c113712789c1
Marius Rackwitz <git@mariusrackwitz.de>
2015-07-12 17:21:35 +0200

    [Project] Setup as universal framework

--- CatchingFire/CatchingFire.h
+++ CatchingFire/CatchingFire.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Marius Rackwitz. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for CatchingFire.
 FOUNDATION_EXPORT double CatchingFireVersionNumber;
